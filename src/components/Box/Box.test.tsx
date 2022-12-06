044254589ccfca47f3a3e8a0e3019839133ce3e1
Yann Cedric <ngadeuyann@yahoo.fr>
2022-12-06 16:50:05 -0500

    Add some tests and workflows

--- src/components/Box/Box.test.tsx
+++ src/components/Box/Box.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 import { Box } from "./Box";
 
@@ -10,5 +11,15 @@ describe("Box", () => {
         <p>Hello</p>
       </Box>
     );
+  });
+
+  test("renders the child", () => {
+    const { getByText } = render(
+      <Box>
+        <p>child</p>
+      </Box>
+    );
+
+    expect(getByText("child")).toBeInTheDocument();
   });
 });
